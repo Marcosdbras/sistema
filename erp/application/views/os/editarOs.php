66bdc29196521d648c7d6733b751ae65c1360ed6
marcosbras <marcosbras@hotmail.com>
2016-11-30 20:29:30 -0200

    Correcao anexo na OS

--- erp/application/views/os/editarOs.php
+++ erp/application/views/os/editarOs.php
@@ -251,16 +251,20 @@
 
                                         if($cont == $flag){
                                            echo '<div style="margin-left: 0" class="span3"><a href="#modal-anexo" imagem="'.$a->idAnexos.'" link="'.$link.'" role="button" class="btn anexo" data-toggle="modal"><img src="'.$thumb.'" alt=""></a>'
-                                                .'<a href="'.base_url().'index.php/os/excluirAnexo/'.$a->idAnexos.'" imagem="'.$a->idAnexos.'"role="button" class="btn apagar" data-toggle="modal"><img src="'.$thumb.'" alt=""></a>   </div>'; 
+                                                .'</div>'; 
                                             
                                            $flag += 4;
                                         }
                                         else{
                                            echo '<div class="span3"><a href="#modal-anexo" imagem="'.$a->idAnexos.'" link="'.$link.'" role="button" class="btn anexo" data-toggle="modal"><img src="'.$thumb.'" alt=""></a>'
-                                                .'<a href="'.base_url().'index.php/os/excluirAnexo/'+$a->idAnexos.'" imagem="'.$a->idAnexos.'" role="button" class="btn apagar" data-toggle="modal"></a>  </div>'; 
+                                                .'</div>'; 
    
                                            
                                         }
+                                        
+                                        
+                                        //.base_url().'index.php/os/excluirAnexo/'.$a->idAnexos
+                                        
                                         $cont ++;
                                     } ?>
                                 </div>
