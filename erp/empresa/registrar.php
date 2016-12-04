60550afbf0b27ef4ef9d62b1c3ac2ed594363c5f
marcosbras <marcosbras@hotmail.com>
2016-12-04 12:59:46 -0200

    registro da empresae usuario principal

--- erp/empresa/registrar.php
+++ erp/empresa/registrar.php
@@ -9,9 +9,66 @@
 
 require_once '../../biblioteca/read.data.php';
 require_once '../../biblioteca/funcoes.php';
-include_once('../valida_dados/verifica.php');
+include_once('../processa/envia_adendo.php');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>
+            
+            
+        </title>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="Nota Fiscal Eletrônica, NFE, ERP, Online, Programa, Administrativo">
+        <meta name="author" content="Marcos Brás">
+        <link rel="icon" href="/image/favicon.ico">
+
+        <title>Ordem de Serviço e venda</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="/stylebootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap theme -->
+        <link href="/stylebootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="/stylebootstrap/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="/stylebootstrap/css/theme.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="/stylebootstrap/js/ie-emulation-modes-warning.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- jquery-ui -->
+        <script src="/stylebootstrap/css/jquery-ui/external/jquery/jquery.js"></script>
+        <script src="/stylebootstrap/js/jquery-ui/jquery-ui.js"></script>
+
+
+        
+        
+        <script src='https://www.google.com/recaptcha/api.js?hl=pt-br'></script>
+    </head>
+    <body>
+        <div class="container-fluid theme-showcase" role="main">
+            
+            
+        </div> 
+    </body>
+</html>   
+            
+
 
 
 
