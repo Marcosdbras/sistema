bc3e6882db250bd19861401c043bc7adfd1b816d
marcosbras <marcosbras@hotmail.com>
2016-12-03 23:02:42 -0200

    página de registro

--- erp/application/views/mapos/login.php
+++ erp/application/views/mapos/login.php
@@ -1,29 +1,29 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-    
-<head>
+
+    <head>
         <title>MAP OS</title><meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap-responsive.min.css" />
-        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/matrix-login.css" />
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/matrix-login.css" />
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-        <script src="<?php echo base_url()?>assets/js/jquery-1.10.2.min.js"></script>
+        <script src="<?php echo base_url() ?>assets/js/jquery-1.10.2.min.js"></script>
     </head>
     <body>
-        <h2>Sistema Emissor de Ordem de Serviço e Vendas</h2>
+        <h2>Ordem de Serviço e Vendas</h2>
         <div id="loginbox">  
-            
-            
-            <form  class="form-vertical" id="formLogin" method="post" action="<?php echo base_url()?>index.php/mapos/verificarLogin">
-                  <?php if($this->session->flashdata('error') != null){?>
-                        <div class="alert alert-danger">
-                          <button type="button" class="close" data-dismiss="alert">&times;</button>
-                          <?php echo $this->session->flashdata('error');?>
-                       </div>
-                  <?php }?>
-                <div class="control-group normal_text"> <h3><img src="<?php echo base_url()?>assets/img/logo.png" alt="Logo" /></h3></div>
+
+
+            <form  class="form-vertical" id="formLogin" method="post" action="<?php echo base_url() ?>index.php/mapos/verificarLogin">
+                <?php if ($this->session->flashdata('error') != null) { ?>
+                    <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        <?php echo $this->session->flashdata('error'); ?>
+                    </div>
+                <?php } ?>
+                <div class="control-group normal_text"> <h3><img src="<?php echo base_url() ?>assets/img/logo.png" alt="Logo" /></h3></div>
                 <div class="control-group">
                     <div class="controls">
                         <div class="main_input_box">
@@ -40,69 +40,87 @@
                 </div>
                 <div class="form-actions" style="text-align: center">
                     <button class="btn btn-info btn-large"/>Acessar</button>
-                    <br />
-                    <a href="<?php echo base_url(); ?>index.php/mapos/registrar.php" class="btn btn-info btn-large">Registre-se</a>
                 </div>
-                
-                
+
+
             </form>
-       
+
         </div>
-        
-        <p>Caso não tenha acesso ao sistema ainda, por gentileza clique em registra-se</p>
-        <p>para que você consiga cadastrar sua empresa e um usuário principal que lhe</p>
-        <p>permitirá realizar cadastro de clientes, produtos, verificar finanças,</p>
-        <p>abrir ordem de serviço, venda, entre outras.</p>
-        
-        
-        <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
-        <script src="<?php echo base_url()?>assets/js/validate.js"></script>
+
+        <div class ="container-fluid">
+
+            <div class="row-fluid">
+
+                <div class="col-xs-5 col-sm-6 col-lg-4">
+                    <a href="<?php echo base_url(); ?>index.php/registrar.php" class="btn btn-info btn-large">Registre-se</a>                    
+
+                </div>
+
+
+                <div class="col-xs-7 col-sm-6 col-lg-8">
+                    <p>Caso não tenha acesso ao sistema ainda, por gentileza clique em registra-se</p>
+                    <p>para que você consiga cadastrar sua empresa e um usuário principal que lhe</p>
+                    <p>permitirá realizar cadastro de clientes, produtos, verificar finanças,</p>
+                    <p>abrir ordem de serviço, venda, entre outras.</p>
+
+
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+
+
+        <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url() ?>assets/js/validate.js"></script>
 
 
 
 
         <script type="text/javascript">
-            $(document).ready(function(){
+            $(document).ready(function () {
 
                 $('#email').focus();
                 $("#formLogin").validate({
-                     rules :{
-                          email: { required: true, email: true},
-                          senha: { required: true}
+                    rules: {
+                        email: {required: true, email: true},
+                        senha: {required: true}
                     },
-                    messages:{
-                          email: { required: 'Campo Requerido.', email: 'Insira Email válido'},
-                          senha: {required: 'Campo Requerido.'}
+                    messages: {
+                        email: {required: 'Campo Requerido.', email: 'Insira Email válido'},
+                        senha: {required: 'Campo Requerido.'}
                     },
-                   submitHandler: function( form ){       
-                         var dados = $( form ).serialize();
-                         
-                    
+                    submitHandler: function (form) {
+                        var dados = $(form).serialize();
+
+
                         $.ajax({
-                          type: "POST",
-                          url: "<?php echo base_url();?>index.php/mapos/verificarLogin?ajax=true",
-                          data: dados,
-                          dataType: 'json',
-                          success: function(data)
-                          {
-                            if(data.result == true){
-                                window.location.href = "<?php echo base_url();?>index.php/mapos";
+                            type: "POST",
+                            url: "<?php echo base_url(); ?>index.php/mapos/verificarLogin?ajax=true",
+                            data: dados,
+                            dataType: 'json',
+                            success: function (data)
+                            {
+                                if (data.result == true) {
+                                    window.location.href = "<?php echo base_url(); ?>index.php/mapos";
+                                } else {
+                                    $('#call-modal').trigger('click');
+                                }
                             }
-                            else{
-                                $('#call-modal').trigger('click');
-                            }
-                          }
-                          });
+                        });
 
-                          return false;
+                        return false;
                     },
-
                     errorClass: "help-inline",
                     errorElement: "span",
-                    highlight:function(element, errorClass, validClass) {
+                    highlight: function (element, errorClass, validClass) {
                         $(element).parents('.control-group').addClass('error');
                     },
-                    unhighlight: function(element, errorClass, validClass) {
+                    unhighlight: function (element, errorClass, validClass) {
                         $(element).parents('.control-group').removeClass('error');
                         $(element).parents('.control-group').addClass('success');
                     }
@@ -117,17 +135,17 @@
         <a href="#notification" id="call-modal" role="button" class="btn" data-toggle="modal" style="display: none ">notification</a>
 
         <div id="notification" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h4 id="myModalLabel">MAP OS</h4>
-          </div>
-          <div class="modal-body">
-            <h5 style="text-align: center">Os dados de acesso estão incorretos, por favor tente novamente!</h5>
-          </div>
-          <div class="modal-footer">
-            <button class="btn btn-primary" data-dismiss="modal" aria-hidden="true">Fechar</button>
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 id="myModalLabel">MAP OS</h4>
+            </div>
+            <div class="modal-body">
+                <h5 style="text-align: center">Os dados de acesso estão incorretos, por favor tente novamente!</h5>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary" data-dismiss="modal" aria-hidden="true">Fechar</button>
 
-          </div>
+            </div>
         </div>
 
 
