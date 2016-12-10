b07140ad22bd308c2e65b399f69df575b95b31e7
marcosbras <marcosbras@hotmail.com>
2016-12-10 20:15:45 -0200

    separação de id das empresas

--- erp/application/controllers/clientes.php
+++ erp/application/controllers/clientes.php
@@ -75,7 +75,7 @@ class Clientes extends CI_Controller {
         } else {
 
             //Author: Marcos Brás--------------------- 
-            $this->db->select('count(*) as totreg');
+            $this->db->select('clientes');
             $this->db->where('idusumestre', $this->session->userdata('idusumestre'));
             $totreg = $this->db->count_all_results()+ 1;
             //--------------------------------------
