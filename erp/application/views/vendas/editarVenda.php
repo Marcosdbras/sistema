7445bad5f97de1df01d7c83168f34a87ed64d677
marcosbras <marcosbras@hotmail.com>
2017-01-07 23:45:57 -0200

    venda

--- erp/application/views/vendas/editarVenda.php
+++ erp/application/views/vendas/editarVenda.php
@@ -8,6 +8,9 @@
 <link rel="stylesheet" href="<?php echo base_url();?>js/jquery-ui/css/smoothness/jquery-ui-1.9.2.custom.css" />
 <script type="text/javascript" src="<?php echo base_url()?>js/jquery-ui/js/jquery-ui-1.9.2.custom.js"></script>
 
+<script  type="text/javascript" src="../stylebootstrap/number/jquery.number.min.js"></script>
+
+
 <?php 
   
   js_aoSairDoCampo();
