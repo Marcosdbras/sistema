6888f655107a717d95380dd62988bab03833b6c5
marcosbras <marcosbras@hotmail.com>
2017-01-11 14:34:51 -0200

    Ajustes

--- erp/application/views/produtos/editarProduto.php
+++ erp/application/views/produtos/editarProduto.php
@@ -28,14 +28,14 @@
                     <div class="control-group">
                         <label for="precoCompra" class="control-label">Preço de Compra<span class="required">*</span></label>
                         <div class="controls">
-                            <input id="precoCompra" class="money" type="text" name="precoCompra" value="<?php echo  number_format($result->precoCompra,',',''); ?>"  />
+                            <input id="precoCompra" class="money" type="text" name="precoCompra" value="<?php echo  number_format($result->precoCompra,2,',',''); ?>"  />
                         </div>
                     </div>
 
                     <div class="control-group">
                         <label for="precoVenda" class="control-label">Preço de Venda<span class="required">*</span></label>
                         <div class="controls">
-                            <input id="precoVenda" class="money" type="text" name="precoVenda" value="<?php echo    number_format($result->precoVenda,',','')    ; ?>"  />
+                            <input id="precoVenda" class="money" type="text" name="precoVenda" value="<?php echo    number_format($result->precoVenda,2,',',''); ?>"  />
                         </div>
                     </div>
 
