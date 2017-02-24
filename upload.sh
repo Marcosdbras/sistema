0027aefc8ea84d3ef5b1e179d59a7eb64c11d3c3
marcosbras <marcosbras@hotmail.com>
2017-02-24 12:04:58 -0300

    upload alteracao padrao

--- upload.sh
+++ upload.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-echo 'copiando arquivos anexos mapos'
+echo 'copiando arquivos anexos mapos de servidor openshift para local...'
 
-scp -r  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/anexos   ~/bkp/anexos
-scp -r  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/arquivos ~/bkp/arquivos
-scp -r  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/uploads  ~/bkp/uploads
+scp -r  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/anexos   ~/bkp/
+scp -r  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/arquivos ~/bkp/
+scp -r  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/uploads  ~/bkp/
+
+echo 'realizando uploads via gitbash...'
+cd ~/sistema
+git add .
+git commit -m "upload alteracao padrao"
+git push origin master
+
+
+echo 'retornando arquivos anexos mapos de local para servidor openshift'
+scp -r  ~/bkp/anexos    5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/anexos   
+scp -r  ~/bkp/arquivos  5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/arquivos 
+scp -r  ~/bkp/uploads   5834494b2d5271f7fe0000cb@sistema-marcosbras.rhcloud.com:app-root/repo/erp/assets/uploads  
+
+
 
 
 
