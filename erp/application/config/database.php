a10126a1f11e081ba00404bcd04156b4a6b62c2d
Marcosdbras <marcosbras@hotmail.com>
2018-09-02 16:09:33 -0300

    Update database.php

--- erp/application/config/database.php
+++ erp/application/config/database.php
@@ -11,7 +11,7 @@ $active_record = TRUE;
 */
 
 //Executar em Absam
-$db['default']['hostname'] = 'localhost'; 
+$db['default']['hostname'] = '167.114.39.253'; 
 $db['default']['username'] = 'admin';
 $db['default']['password'] = 'bios2805';
 $db['default']['database'] = 'sistema_erp';
